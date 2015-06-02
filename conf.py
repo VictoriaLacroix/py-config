f9f5205b8d4d56c956250078b8d2d8f3af263eb1
Victory000 <victoria.a.lacroix@gmail.com>
2015-06-02 09:39:11 -0400

    Added config helper.
    This module creates a new config object from python's library, but automatically converts values to the correct type.
    Signed-off-by: Victory000 <victoria.a.lacroix@gmail.com>

---
+++ conf.py
@@ -0,0 +1,56 @@
+import sys
+# Configparser was renamed between versions, so let's import the right one.
+if sys.version_info[0] == 2:
+    from ConfigParser import ConfigParser
+elif sys.version_info[0] == 3:
+    from configparser import ConfigParser
+import re
+
+class ConfigFile():
+    """
+    Adapted from a helper method from Python Wiki.
+    This class will hold a config file, and attempt to
+    automatically convert values to ints or booleans.
+    """
+    def __init__(self, path):
+        self.conf = ConfigParser()
+        self.conf.read(path)
+
+    def getsection(self, section):
+        """
+        Returns an entire section in a dict.
+        The dict's keys will be uppercase, for convenience. 
+        """
+        keys = {}
+        try:
+            options = self.conf.options(section)
+        except:
+            return {}
+
+        for opt in options:
+            key = opt.upper()
+            try:
+                keys[key] = self.get(section, opt)
+            except:
+                keys[key] = None
+        return keys
+
+    def get(self, section, opt):
+        """
+        Gets a config value. This value will automatically
+        be converted to a boolean, float or int.
+        """
+        try:
+            key = self.conf.get(section, opt)
+            if key == 'True':
+                return True
+            elif key == 'False':
+                return False
+            elif re.match('^[0-9]+$', key):
+                return self.conf.getint(section, opt)
+            elif re.match('^[0-9]+\.[0-9]+$', key):
+                return self.conf.getfloat(section, opt)
+            else:
+                return key
+        except:
+            return None
